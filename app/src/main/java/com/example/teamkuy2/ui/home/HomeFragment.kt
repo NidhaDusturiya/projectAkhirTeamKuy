8555892188914ff7c083bb19e8c5bc1f95148c7d
NidhaDusturiya <nidhadusturiya4@gmail.com>
2023-10-30 19:22:31 +0700

    recyclerview

--- app/src/main/java/com/example/teamkuy2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/teamkuy2/ui/home/HomeFragment.kt
@@ -1,21 +1,31 @@
-package com.example.teamkuy2.ui.home
+package com.example.teamkuy.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.teamkuy2.databinding.FragmentHomeBinding
+import com.example.teamkuy2.ui.model.DataItem
+import com.example.teamkuy2.ui.model.ResponseUser
+import com.example.teamkuy.ui.network.ApiConfig
+import com.example.teamkuy2.ui.home.HomeViewModel
+import com.example.teamkuy2.ui.home.UserAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var adapter: UserAdapter
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,12 +38,36 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        adapter = UserAdapter(mutableListOf())
+        val recyclerView = binding.rvUsers
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+
+
+        getUser()
         return root
     }
+
+    private fun getUser(){
+        val client = ApiConfig.getApiService().getListUsers("1")
+
+        client.enqueue(object : Callback<ResponseUser> {
+            override fun onResponse(call: Call<ResponseUser>, response: Response<ResponseUser>){
+                if (response.isSuccessful) {
+                    val dataArray = response.body()?.data as List<DataItem>
+                    for (data in dataArray) {
+                        adapter.addUser(data)
+                    }
+                }
+            }
+            override fun onFailure(call: Call<ResponseUser>, t: Throwable) {
+                Toast.makeText(requireContext(), t.message, Toast.LENGTH_SHORT).show()
+                t.printStackTrace()
+            }
+        })
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
