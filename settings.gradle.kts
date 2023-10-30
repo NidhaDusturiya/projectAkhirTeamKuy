2ddac397176055fc8b037bcbe139443829a1a64d
NidhaDusturiya <nidhadusturiya4@gmail.com>
2023-10-30 17:04:20 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TeamKuy2"
+include(":app")
+ 
