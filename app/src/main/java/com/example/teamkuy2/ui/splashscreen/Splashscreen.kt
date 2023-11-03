3859b3e8b5dd01ff25624fe9d074edad502e21e6
NidhaDusturiya <nidhadusturiya4@gmail.com>
2023-11-03 09:33:39 +0700

    splash

--- app/src/main/java/com/example/teamkuy2/ui/splashscreen/Splashscreen.kt
+++ app/src/main/java/com/example/teamkuy2/ui/splashscreen/Splashscreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.teamkuy2.ui.OnBoarding
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,7 @@ class Splashscreen : AppCompatActivity() {
 
         splash.alpha = 0f
         splash.animate().setDuration(1000).alpha(1f).withEndAction {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, OnBoarding::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
