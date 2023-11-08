6c2ebf9f71d6c9c9c8468c27ed41034d64fabbc8
mrayhantazakka <rayhantazakka1123@gmail.com>
2023-11-08 15:12:49 +0700

    layout favorite

--- app/src/main/java/com/example/teamkuy2/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/teamkuy2/ui/favorite/FavoriteViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.teamkuy2.ui.notifications
+package com.example.teamkuy2.ui.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
